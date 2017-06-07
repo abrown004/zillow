402fd79015ea946aaca7b3e7056a370dda3c93ee
abrown004 <abrown004@gmail.com>
2017-06-06 23:09:15 -0700

    Add data import

---
+++ zillow_standard_error.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 06 22:31:06 2017
+
+@author: abrow
+"""
+
+import pandas as pd
+
+### Load the dictionary containing the dataset
+df = pd.read_csv("properties_2016.csv")
